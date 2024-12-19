4f8b74a2c91b71233ac548b39631154a67433ac9
Mateusz Maślanka <91078604+maslankamateusz@users.noreply.github.com>
2024-12-19 20:56:28 +0100

    feat: add schedule retrieval endpoint and enhance GTFS data processing

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Query
+from fastapi.encoders import jsonable_encoder
 from ..services.gtfs_data_loader import load_gtfs_data
-from ..services.gtfs_processing import get_routes_list, get_stops_list
+from ..services.gtfs_processing import get_routes_list, get_stops_list, get_schedule_data
+import pandas as pd
 
 router = APIRouter()
 
@@ -14,21 +16,47 @@ class GTFSData:
 def get_gtfs_data() -> GTFSData:
     return GTFSData()
 
+def convert_schedule_for_json(schedule_data):
+    if isinstance(schedule_data, pd.DataFrame):
+        return schedule_data.astype(object).where(pd.notnull(schedule_data), None).to_dict(orient='records')
+    elif isinstance(schedule_data, list):
+        return [convert_schedule_for_json(data) for data in schedule_data]
+    elif isinstance(schedule_data, dict):
+        return {key: convert_schedule_for_json(value) for key, value in schedule_data.items()}
+    elif isinstance(schedule_data, (int, float, str, bool)) or schedule_data is None:
+        return schedule_data
+    else:
+        try:
+            return jsonable_encoder(schedule_data)
+        except Exception:
+            return str(schedule_data) 
+
 @router.get("/api/routes")
 async def get_routes(gtfs: GTFSData = Depends(get_gtfs_data)):
     data = gtfs.get_data()
     routes = get_routes_list(data)
-    return routes
+    return jsonable_encoder(routes)
 
 @router.get("/api/stops")
 async def get_stops(
     gtfs: GTFSData = Depends(get_gtfs_data),
-    route_number: str = Query(..., description="Route number"),
-    direction: int = Query(..., description="Direction")
+    route_number: str = Query(...),
+    direction: int = Query(...)
 ):
     data = gtfs.get_data()
     stops = get_stops_list(data, route_number, direction)
-    return stops
+    return jsonable_encoder(stops)
+
+@router.get("/api/schedule")
+async def get_schedule(
+    gtfs: GTFSData = Depends(get_gtfs_data),
+    route_id: str = Query(...),
+    vehicle_type: str = Query(...)
+):
+    data = gtfs.get_data()
+    schedule = get_schedule_data(data, route_id, vehicle_type)
+    json_serializable_schedule = convert_schedule_for_json(schedule)
+    return json_serializable_schedule
 
 def configure_routes(app):
     app.include_router(router)
--- backend/services/gtfs_processing.py
+++ backend/services/gtfs_processing.py
@@ -36,7 +36,57 @@ def get_stops_list(gtfs_data, route_number, direction):
 
     stops_for_all_trips = stop_times.loc[trip_ids].reset_index().merge(stops, on='stop_id')
     stops = stops_for_all_trips[['stop_id', 'stop_name']].drop_duplicates().to_dict(orient='records')
-    
-    print(stops)
     return stops
 
+def get_schedule_data(gtfs_data, route_id, vehicle_type='bus'):
+    days_of_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    if vehicle_type == 'bus':
+        trips_data = gtfs_data['trips_a']
+        stop_times_data = gtfs_data['stop_times_a']
+        calendar_data = gtfs_data['calendar_a']
+    elif vehicle_type == 'tram':
+        trips_data = gtfs_data['trips_t']
+        stop_times_data = gtfs_data['stop_times_t']
+        calendar_data = gtfs_data['calendar_t']
+    else:
+        raise ValueError("Invalid vehicle type. Must be 'bus' or 'tram'.")
+
+    filtered_data = trips_data[trips_data['route_id'] == route_id].copy()
+    if 'trip_id' in filtered_data.index.names:
+        filtered_data.reset_index(inplace=True)
+    
+    filtered_data['block_prefix'] = filtered_data['trip_id'].str.split('_').str[:2].str.join('_')
+    unique_blocks = filtered_data[['block_prefix']].drop_duplicates().reset_index(drop=True)
+        
+    block_prefixes = unique_blocks['block_prefix']
+
+    if 'trip_id' in stop_times_data.index.names:
+        stop_times_data.reset_index(inplace=True)
+
+    route_schedule_list = []
+
+    for block_prefix in block_prefixes:
+        block_filtered_trips = trips_data[trips_data.index.str.startswith(block_prefix + '_t')]
+
+        first_trip_id = block_filtered_trips.iloc[0].name
+        filtred_start_time_data = stop_times_data[stop_times_data['trip_id'] == first_trip_id]
+        start_time = filtred_start_time_data.departure_time.values[0]
+        last_trip_id = block_filtered_trips.iloc[-1].name
+        filtred_end_time_data = stop_times_data[stop_times_data['trip_id'] == last_trip_id]
+        end_time = filtred_end_time_data.departure_time.values[-1]
+
+        service_id = block_filtered_trips["service_id"].values[0]
+
+        service_day = calendar_data[calendar_data['service_id'] == service_id].copy()
+        days_with_service = service_day[days_of_week].loc[:, service_day[days_of_week].iloc[0] == 1].columns.tolist()
+        block_filtered_trips_with_index = block_filtered_trips.reset_index()
+
+        schedule_dict = {
+            'block_prefix': block_prefix,
+            'start_time': start_time,
+            'end_time': end_time,
+            'route_schedule': block_filtered_trips_with_index,
+            'service_days': days_with_service
+        }
+        route_schedule_list.append(schedule_dict)
+    return route_schedule_list
